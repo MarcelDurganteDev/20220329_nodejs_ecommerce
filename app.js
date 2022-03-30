d3dbb6a6a45c0dfacd66210afe1aaf2f060a1e48
MarcelDurgante.Dev <info@marceldurgante.dev>
2022-03-30 11:26:36 +0200

    mongoose package installed, DB Connected successful

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const express = require( 'express' );
-const app = express();
-require( 'dotenv' ).config();
+const express = require('express');
+const mongoose = require('mongoose');
 
-// route
-app.get( '/', ( req, res ) => {
-    res.send( 'Hello from Node Marcel' )
-} );
+const dotenv = require('dotenv');
+dotenv.config();
+// app
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.send('Hello from Node Marcel');
+});
 
 const port = process.env.PORT || 8000;
 
-app.listen( port, () => {
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
 
+// // db connection
+
+mongoose
+    .connect( process.env.DATABASE, {} )
+    .then( () => {
+        console.log( 'DB Connected' );
+    } )
+    .catch( ( err ) => console.log( 'DB error => ', err ) );
